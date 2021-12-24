7afced4d4cf7011c6a040fdfa92d24e2751798ff
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-24 13:40:51 -0600

    Create Application.java

---
+++ spring/Application.java
@@ -0,0 +1,18 @@
+package com.dz_fs_dev.biology.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
