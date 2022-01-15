e267a26e4cc684617780e70f9b54501928fdb25a
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2022-01-15 11:08:06 -0600

    Create RESTController_DNA.java

---
+++ spring/RESTController_DNA.java
@@ -0,0 +1,37 @@
+/*  Original Licensing Copyright
+ * 
+ *  DNA REST Service Controller.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.biology.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * DNA REST Service Controller.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+@RestController
+@RequestMapping("api/v1/DNA")
+public class RESTController_DNA {
+	@Autowired
+	DNAservice dnasvc;
+}
